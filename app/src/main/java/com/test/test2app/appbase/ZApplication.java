55e9546f4a0c3d23f446e9a1449e5e914a127380
zhaoyuntao <zhaoyuntao@yeecall.com>
2020-03-31 14:47:18 +0400

    20200331 threadPool.remove

---
+++ app/src/main/java/com/test/test2app/appbase/ZApplication.java
@@ -0,0 +1,19 @@
+package com.test.test2app.appbase;
+
+import android.app.Application;
+
+import com.test.test2app.threadpool.Utilities;
+
+/**
+ * created by zhaoyuntao
+ * on 2020-03-31
+ * description:
+ */
+public class ZApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Utilities.setContext(this.getApplicationContext());
+    }
+}
